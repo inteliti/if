8a017fb5d6945fba1fce8820d5d903a5cf5afb96
guacho <guachos2003@gmail.com>
2016-02-02 16:37:38 -0430

    Separando carga de archivos CSS y JS esto repercute en rendimiento

---
+++ application/core/IF_CSS_Loader.php
@@ -0,0 +1,10 @@
+<!-- Third party libs -->
+<link rel='stylesheet' type='text/css' 
+	  href='<?=SHARED_URL; ?>css/third/bootstrap/bootstrap.min.css' />
+<link rel='stylesheet' type='text/css' 
+	  href='<?=SHARED_URL; ?>css/third/bootstrap/bootstrap-theme.min.css' />
+<link rel='stylesheet' type='text/css'
+	  href='<?=SHARED_URL; ?>css/third/jquery.ui.min.css' />
+<link rel='stylesheet' type='text/css'
+	  href='<?=SHARED_URL; ?>css/third/icons.css' />
+
