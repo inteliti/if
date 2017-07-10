2b5965d9e3ce4d01c566da280cc9ee720beb335d
guacho <guachos2003@gmail.com>
2017-07-10 10:16:09 -0430

    10-07-2017: haciendo commit nantes de pull

--- application/libraries/IF_PHPMailer.php
+++ application/libraries/IF_PHPMailer.php
@@ -1,8 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class IF_PHPMailer {
-    public function IF_PHPMailer()
+	function __construct()
 	{
-        require_once(THIRD_PATH.'php_mailer/PHPMailerAutoload.php');
-    }
+		require_once(THIRD_PATH.'php_mailer/PHPMailerAutoload.php');
+	}
 }
